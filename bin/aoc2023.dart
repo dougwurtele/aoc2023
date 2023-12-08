c4aa3d59301faa4f3f72c8532b9eb8ca24b59058
Doug <15134715+dougwurtele@users.noreply.github.com>
2023-12-08 00:39:40 -0500

    Day 8

--- bin/aoc2023.dart
+++ bin/aoc2023.dart
@@ -1,9 +1,9 @@
-import 'package:aoc2023/day7.dart' as day;
+import 'package:aoc2023/day8.dart' as day;
 
 void main(List<String> arguments) async {
   final start = DateTime.timestamp();
   final solution = await day.part2();
-  print('AOC 2023 - Day 7 - Part 2: $solution');
+  print('AOC 2023 - Day 8 - Part 2: $solution');
   final time = DateTime.timestamp().difference(start).inMilliseconds;
   print('Total time: $time ms');
 }
---
+++ lib/day8.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+import 'package:dart_numerics/dart_numerics.dart' as numerics;
+
+Future<int> part1() async {
+  final input = await File('assets/day8/input.txt').readAsLines();
+  final List<String> instructions = input[0].split('');
+  final List<Node> nodes = [];
+  for (int i = 2; i < input.length; i++) {
+    nodes.add(Node.fromString(input[i]));
+  }
+  for (final node in nodes) {
+    node.left = nodes.firstWhere((element) => element.label == node.leftLabel);
+    node.right = nodes.firstWhere((element) => element.label == node.rightLabel);
+  }
+  int step = 0;
+  Node current = nodes.firstWhere((element) => element.label == 'AAA');
+  while (current.label != 'ZZZ') {
+    String instruction = instructions[step % instructions.length];
+    if (instruction == 'L') {
+      current = current.left;
+    } else if (instruction == 'R') {
+      current = current.right;
+    }
+    step++;
+  }
+  return step;
+}
+
+Future<int> part2() async {
+  final input = await File('assets/day8/input.txt').readAsLines();
+  final List<String> instructions = input[0].split('');
+  final List<Node> nodes = [];
+  for (int i = 2; i < input.length; i++) {
+    nodes.add(Node.fromString(input[i]));
+  }
+  for (final node in nodes) {
+    node.left = nodes.firstWhere((element) => element.label == node.leftLabel);
+    node.right = nodes.firstWhere((element) => element.label == node.rightLabel);
+  }
+  List<Node> startingNodes = nodes.where((element) => element.isStartNode).toList();
+  Map<Node, int> solutions = {};
+  for (var node in startingNodes) {
+    int step = 0;
+    Node current = node;
+    while (!current.isEndNode) {
+      String instruction = instructions[step % instructions.length];
+      if (instruction == 'L') {
+        current = current.left;
+      } else if (instruction == 'R') {
+        current = current.right;
+      }
+      step++;
+    }
+    solutions[node] = step;
+  }
+  return solutions.values.reduce((value, element) => numerics.leastCommonMultiple(value, element));
+}
+
+class Node {
+  Node.fromString(String input)
+      : label = input.split(' = ')[0],
+        leftLabel = input.split(' = (')[1].split(', ')[0],
+        rightLabel = input.split(' = (')[1].split(', ')[1].substring(0, 3);
+
+  final String label;
+  final String leftLabel;
+  final String rightLabel;
+  late Node left;
+  late Node right;
+
+  bool get isStartNode => label.endsWith('A');
+  bool get isEndNode => label.endsWith('Z');
+
+  @override
+  bool operator ==(Object other) => other is Node && label == other.label;
+
+  @override
+  int get hashCode => label.hashCode;
+
+  @override
+  String toString() => label;
+}
