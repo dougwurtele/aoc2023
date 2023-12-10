4deec5a8626cb8b355291f5ea660220fd5f8e333
Doug <15134715+dougwurtele@users.noreply.github.com>
2023-12-09 21:32:45 -0500

    Optimizing day1 part2

--- lib/day1.dart
+++ lib/day1.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:math';
 
 int part1() {
   final input = File('assets/day1/input.txt');
@@ -17,98 +16,10 @@ int part1() {
 }
 
 int part2() {
-  final input = File('assets/day1/input.txt');
-  final data = input.readAsLinesSync();
-  final List<List<int>> values = data.map((e) {
-    var result = e;
-    for (int i = 0;
-        i < result.length &&
-            RegExp(r'^\d').stringMatch(result.substring(i)) == null;
-        i++) {
-      int initial = result.length;
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^one'), '1');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^two'), '2');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^three'), '3');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^four'), '4');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^five'), '5');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^six'), '6');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^seven'), '7');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^eight'), '8');
-      result = result.substring(0, i) +
-          result.substring(i).replaceFirst(RegExp(r'^nine'), '9');
-      int updated = result.length;
-      if (updated < initial) {
-        break;
-      }
-    }
-    for (int i = 0;
-        i < result.length &&
-            RegExp(r'^\d').stringMatch(result.substring(result.length - i)) == null;
-        i++) {
-      int initial = result.length;
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^one'), '1');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^two'), '2');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^three'), '3');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^four'), '4');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^five'), '5');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^six'), '6');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^seven'), '7');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^eight'), '8');
-      result = result.substring(0, max(0, result.length - i)) +
-          result
-              .substring(max(0, result.length - i))
-              .replaceFirst(RegExp(r'^nine'), '9');
-      int updated = result.length;
-      if (updated < initial) {
-        break;
-      }
-    }
-    return result
-        .split('')
-        .where((element) => int.tryParse(element) != null)
-        .map((element) => int.parse(element))
-        .toList();
-  }).toList();
-  return values.map((e) => e.first * 10 + e.last).reduce((value, element) => value + element);
-}
-
-int part2Regex() {
   final input = File('assets/day1/input.txt').readAsLinesSync();
   final search = RegExp(r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))');
   return input.map((e) => search.allMatches(e))
-  .map((e) {
+      .map((e) {
     var result = '';
     for (var match in [e.first.group(1), e.last.group(1)]) {
       result += match!
@@ -124,6 +35,6 @@ int part2Regex() {
     }
     return result;
   })
-  .map((e) => int.parse(e))
-  .reduce((value, element) => value + element);
+      .map((e) => int.parse(e))
+      .reduce((value, element) => value + element);
 }
